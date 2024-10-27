5cbb354be73c3c61edc257d8f59905eddf5f9278
lboss39 <ismatach5@gmail.com>
2024-10-27 01:08:28 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
-const path = require("path");
-
-// Prefixo dos comandos.
+// Prefixo dos comandos
 exports.PREFIX = "/";
 
 // Emoji do bot (mude se preferir).
@@ -10,28 +8,7 @@ exports.BOT_EMOJI = "🤖";
 exports.BOT_NAME = "Takeshi Bot";
 
 // Número do bot. Coloque o número do bot (apenas números).
-exports.BOT_NUMBER = "5511920202020";
+exports.BOT_NUMBER = "+34 682 62 21 95";
 
 // Número do dono do bot. Coloque o número do dono do bot (apenas números).
-exports.OWNER_NUMBER = "5511999999999";
-
-// Diretório dos comandos
-exports.COMMANDS_DIR = path.join(__dirname, "commands");
-
-// Diretório de arquivos de mídia.
-exports.ASSETS_DIR = path.resolve(__dirname, "..", "assets");
-
-// Diretório de arquivos temporários.
-exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
-
-// Timeout em milissegundos por evento (evita banimento).
-exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 300;
-
-// Plataforma de API's
-exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
-
-// Obtenha seu token, criando uma conta em: https://api.spiderx.com.br.
-exports.SPIDER_API_TOKEN = "seu_token_aqui";
-
-// Caso queira responder apenas um grupo específico, coloque o ID dele aqui (ex: 120363023799506419@g.us). Apenas para testes internos!
-exports.ONLY_GROUP_ID = "";
+exports.OWNER_NUMBER = "+34 682 62 21 95";
